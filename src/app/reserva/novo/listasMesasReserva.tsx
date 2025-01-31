35ed72f8d9dd36e0c82d312c8fd34dad9fdc743b
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2025-01-31 15:57:23 -0400

    cadastro das reservas

--- src/app/reserva/novo/listasMesasReserva.tsx
+++ src/app/reserva/novo/listasMesasReserva.tsx
@@ -1,27 +1,82 @@
-import Mesa from "@/app/Interfaces/mesa"
-import ModalMap from "@/app/Componentes/modalMap"
+'use client'
+
+import Mesa from "@/app/Interfaces/mesa";
+import ModalMap from "@/app/Componentes/modalMap";
+import Reserva from "@/app/utils/reservas"
+import { use, useState } from 'react';
+
 type ListMesasReservaProps = {
-    mesas: Mesa []
+    mesas: Mesa[]
 }
-export function ListMesasReserva({mesas}: ListMesasReservaProps){
-    return(
+export function ListMesasReserva({ mesas }: ListMesasReservaProps) {
+
+    const [data, setData] = useState('');
+    const [reservas, setReservas] = useState<Reserva[] | null>(null);
+    const [loadReservas, setLoadReservas] = useState(false);
+    const [mesas2, setMesas2] = useState<Mesa | null>(null)
+
+    async function handleFecthData() {
+        setLoadReservas(true)
+        await new Promise(reseolve => setTimeout(reseolve, 3000))
+        const res = await a(data)
+        setReservas(res)
+        setLoadReservas(false)
+    };
+    return (
         <div className="w-full lg:w-3/4 flex flex-col lg:flex-row p-4 rounded-lg overflow-hidden">
             <div className="w-full bg-white p-6 rounded-lg shadow-lg">
                 <h2 className="text-xl font-bold mb-4">Mapa Restaurante:</h2>
-                <ModalMap/> 
-            {
-                mesas.map(mesa => {
-                    return (
-                        <button
-                        key={mesa.id}
-                        className="p-4 text-white bg-indigo-500 rounded-lg hover:bg-indigo-600 focus:outline-none focus: bg-indigo-700"
-                        >
-                           <p>{mesa.codigo}</p>
-                        </button>
-                    )
-                })
-            }
+                <ModalMap />
+                <h2 className="text-xl font-bold mb-4">Fazer reserva:</h2>
+                <div className="flex w-full gap-4">
+                    <input
+                        type="date"
+                        value={data}
+                        onChange={e => setData(e.target.value)}
+                        className="p-2 border rounded w-5/6"
+                    />
+                    <button type="button"
+                        onClick={handleFecthData}
+                        className="p-4 text-white bg-indigo-500 rounded-lg hover:bg-indigo-600 focus:outline-none focus: bg-indigo-700 w-1/6"
+                    >
+                        Buscar
+                    </button>
+
+                </div>
+                {loadReservas && <p className="text-center mt-4">Carregando Mesas...</p>}
+                <div className="grid grid cols-4 lg:grid-cols-12 gap-4 mt-4">
+
+                    {reservas && !loadReservas &&
+                        mesas.map(mesa => {
+                            if (reservas.find(reserva => reserva.mesaId === mesa.id))
+                                return (
+                                    <button
+                                        key={mesa.id}
+                                        className="p-4 text-white bg-indigo-500 rounded-lg"
+                                    >
+                                        <p>{mesa.codigo}</p>
+                                    </button>
+                                )
+                            return (
+                                <button
+                                    key={mesa.id}
+                                    className="p-4 text-white bg-indigo-500 rounded-lg hover:bg-indigo-600 focus:outline-none focus: bg-indigo-700"
+                                >
+                                    <p>{mesa.codigo}</p>
+                                </button>
+                            )
+                        })
+                    }
+                </div>
             </div>
+            {mesas2 && (
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
+            <div className="bg-white rounded-lg shadow-lg p-6 w-1/3">
+
+        </div>  
+        
+     </div>
+   )}
         </div>
     )
 }
